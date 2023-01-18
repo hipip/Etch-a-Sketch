27249fd039a935701be06a27f1e34c836c0eacca
somehowalive <slow.optimism@gmail.com>
2023-01-18 21:31:29 +0100

    "finished a working simple version"

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+var mouseClicked = false;
+var size = 32;
+
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    for (let i = 0; i < 16; i++) {
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         const newSquare = document.createElement("div");
-        newSquare.classList.add("square");
-        newSquare.addEventListener("mouseover", handleClick);
-        newSquare.addEventListener("mousedown", handleClick);
+        newSquare.style.width = `calc(var(--size) / ${size}`;
+        newSquare.style.height = `calc(var(--size) / ${size}`;
+        newSquare.addEventListener("mouseover", changeColor);
+        newSquare.addEventListener("mousedown", changeColor2);
         container.append(newSquare);
     }
 }
+
+container.addEventListener("mousedown", () => (mouseClicked = true));
+container.addEventListener("mouseup", () => (mouseClicked = false));
+
+createGrid(16);
 
 function random(n) {
     return Math.floor(Math.random() * (n + 1));
@@ -16,7 +25,9 @@ function randomColor() {
     return `rgb(${random(255)},${random(255)},${random(255)})`;
 }
 
-function handleClick(e) {
-    e.preventDefault();
-    if (e.type === "mousedown") e.target.style.backgroundColor = randomColor();
+function changeColor(e) {
+    if (mouseClicked) e.target.style.backgroundColor = randomColor();
+}
+function changeColor2(e) {
+    e.target.style.backgroundColor = randomColor();
 }
